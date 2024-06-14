14b95aeb943587aef093e2e5b2603395a0b8fbb9
songorren02 <david.amador@enti.cat>
2024-06-15 01:22:49 +0200

    Mostrar el tablero acabado

--- damas/damas/Inicializacion.cpp
+++ damas/damas/Inicializacion.cpp
@@ -10,3 +10,16 @@ void InicializarTablero(char* tablero, char vacio) {
 		}
 	}
 }
+
+void MostrarTablero(char* tablero) {
+	//Recorremos las filas
+	for (short i = 0; i < AMOUNT_FILAS; i++) {
+		//Recorremos las columnas
+		for (short j = 0; j < AMOUNT_COLUMNAS; j++) {
+			//Mostramos el contenido en [i][j]
+			std::cout << tablero[i * AMOUNT_COLUMNAS + j];
+		}
+		//Para que quede como un cuadrado, cada vez que acabe de recorrer las columnas debemos hacer un salto de linea
+		std::cout << std::endl;
+	}
+}
